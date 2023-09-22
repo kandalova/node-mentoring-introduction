b07eb5da6cfe6d0e76e01369e0b35ef51def8ce6
Leyla Kandalova <leyla_kandalova@epam.com>
2023-09-22 18:04:14 +0600

    Refactor ifs, function, constsnts; remove logs

--- cli_server.js
+++ cli_server.js
@@ -1,33 +1,33 @@
 const { exec } = require('node:child_process');
 const os = require('node:os'); 
 const fs = require('fs');
+const readline = require('readline');
+
+const COMMANDS = {
+	win: `powershell "Get-Process | Sort-Object CPU -Descending | Select-Object -Property Name, CPU, WorkingSet -First 1 | ForEach-Object { $_.Name + ' ' + $_.CPU + ' ' + $_.WorkingSet }"`,
+	unix: `ps -A -o %cpu,%mem,comm | sort -nr | head -n 1`
+}
 
 
-function startServer(){
-	const commands = {
-		win: `powershell "Get-Process | Sort-Object CPU -Descending | Select-Object -Property Name, CPU, WorkingSet -First 1 | ForEach-Object { $_.Name + ' ' + $_.CPU + ' ' + $_.WorkingSet }"`,
-		unix: `ps -A -o %cpu,%mem,comm | sort -nr | head -n 1`
-	}
+function startServer(){	
 	const userOS = os.type();
-	const userCommand = userOS==='Windows_NT' ? commands.win : commands.unix;
+	const userCommand = userOS==='Windows_NT' ? COMMANDS.win : COMMANDS.unix;
 
 	let lastMinuteTime = Date.now();
 	setInterval(()=>execCommand(), 1000);
 
 	function execCommand(){
 		const currentTime = Date.now();
-		const isTimeToLog = ((currentTime - lastMinuteTime) / 1000) >= 60;
-	
-		if (isTimeToLog) {
-			lastMinuteTime = currentTime;	
-		}
+		const isTimeToLog = oneMinutePassed(currentTime, lastMinuteTime);
 	
 		exec(userCommand, (err, stdout, stderr)=>{
 			if(err) throw err;
-			console.clear();
-			console.log(stdout);
-	
+			readline.cursorTo(process.stdout, 0, 0)
+ 			readline.clearScreenDown(process.stdout)
+			process.stdout.write(stdout);
+
 			if(isTimeToLog){
+				lastMinuteTime = currentTime;	
 				const logMsg = `${lastMinuteTime}: ${stdout}\r`;
 				fs.appendFile('activityMonitor.log', logMsg, (err) => {
 					if (err) throw err;
@@ -35,6 +35,10 @@ function startServer(){
 			}
 		})
 	}
+}
+
+function oneMinutePassed(currentTime, lastMinuteTime){
+	((currentTime - lastMinuteTime) / 1000) >= 60;
 }
 
 startServer();
